64aa7d0df4272b14a6b2107d1dd88871aa8be12d
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2022-11-27 17:47:45 +0200

    Add project files.

---
+++ lab1ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab1ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex1
+Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+paralelipiped dreptunghic, va afisa volumul lui
+• Google: volumul paralelipipedului dreptunghic
+
+             volum = latime * lungime * inaltime;
+             */
+            Console.WriteLine("Acest program va calcula volumul paralelipipedului dreptunghic");
+
+            Console.WriteLine("Introduceti latimea:");
+            int width = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Introduceti lungimea:");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti inaltime:");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Volumum paralelipipedului dreptunghic este: " + width * length * height);
+        }
+    }
+}
